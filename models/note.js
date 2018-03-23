46c67756914bf070a69c3a462168bfdd5f570006
George Colon <geocolon@gmail.com>
2018-03-23 14:09:31 -0700

    this is new

---
+++ models/note.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+  title: { type: String },
+  content: { type: String },
+  created: { type: Date, default: Date.now },
+  folderId: { type: mongoose.Schema.Types.ObjectId, ref: 'Folder' }
+});
+
+noteSchema.set('toObject', {
+  transform: function (doc, ret) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+
+module.exports = mongoose.model('Note', noteSchema);
